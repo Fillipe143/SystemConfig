60256e34188fe43317bb4c4c4e599a9b7501cdbf
Fillipe143 <fillipefbatist@gmail.com>
2026-02-21 12:59:20 -0300

    Configurado lsp
    - Adicionado lsp para rust
    - Configurado keybinds pro lsp
    - Configurado ibl para não mostrar a identação em linhas vazias

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -1,9 +1,13 @@
--- plugins.lua (lazy.nvim)
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {
-        indent = { char = "·", tab_char = "·", },
-    },
-}
+    opts = { indent = { char = "·", tab_char = "·" }, },
+    config = function(_, opts)
+        require("ibl").setup(opts)
 
+        local hooks = require("ibl.hooks")
+        hooks.register(hooks.type.SKIP_LINE, function(_, _, _, line)
+            return line:match("^$")
+        end)
+    end,
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,19 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
+        -- Keybinds
+        local opts = { noremap = true, silent = true }
+        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)                                           -- Ir para definição
+        vim.keymap.set("n", "<leader>lx", vim.lsp.buf.declaration, opts)                                          -- Ir para declaração
+        vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, opts)                                       -- Ir para implementação
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)                                           -- Ver referências
+        vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, opts)                                        -- Abrir diagnóstico flutuante
+        vim.keymap.set("n", "<leader>lj", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)  -- Próximo diagnóstico
+        vim.keymap.set("n", "<leader>lk", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts) -- Diagnóstico anterior
+        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)                                          -- Code actions
+        vim.keymap.set("n", "<leader>lm", vim.lsp.buf.rename, opts)                                               -- Renomear
+        vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, opts)              -- Formatar código
+
         -- Lua
         vim.lsp.config("lua_ls", require("plugins.lsp.lua_ls"))
         vim.lsp.enable("lua_ls")
@@ -12,8 +25,7 @@ return {
         vim.lsp.enable("html")
         vim.lsp.enable("emmet_language_server")
 
-        vim.keymap.set("n", "<leader>ai", function()
-            vim.lsp.buf.format({ async = true })
-        end)
+        -- Rust
+        vim.lsp.enable("rust_analyzer")
     end,
 }
